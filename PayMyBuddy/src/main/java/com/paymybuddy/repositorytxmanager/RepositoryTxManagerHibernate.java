7d90b35e65323de060cd561d6e6813bb83671b6a
olivier.bonheur <olivier.bonheur@gmail.com>
2020-07-14 00:57:50 +0200

    Work on Repository Tx manager

--- PayMyBuddy/src/main/java/com/paymybuddy/repositorytxmanager/RepositoryTxManagerHibernate.java
+++ PayMyBuddy/src/main/java/com/paymybuddy/repositorytxmanager/RepositoryTxManagerHibernate.java
@@ -1,13 +1,12 @@
 package com.paymybuddy.repositorytxmanager;
 
-import java.io.File;
+import java.util.Properties;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,26 +21,26 @@ public class RepositoryTxManagerHibernate {
 
 	private Transaction currentTx;
 
-	private String hibernateConfigurationFile;
+	private String paymybuddyPropertiesFile;
 
 	private static RepositoryTxManagerHibernate repositoryTxManagerHibernate = null;
 
-	private RepositoryTxManagerHibernate(String configFile) {
-		this.hibernateConfigurationFile = configFile;
+	private RepositoryTxManagerHibernate(String paymybuddyPropertiesFile) {
+		this.paymybuddyPropertiesFile = paymybuddyPropertiesFile;
 	}
 
 	/**
 	 * Create an instance of RepositoryTxManagerHibernate, if not already exist.
 	 * 
-	 * @param configurationFile The path of the Hibernate configuration file
+	 * @param paymybuddyPropertiesFile The path of the paymybuddy properties file
 	 * 
 	 * @return The RepositoryTxManagerHibernate
 	 */
-	public static RepositoryTxManagerHibernate getRepositoryTxManagerHibernate(String configurationFile) {
+	public static RepositoryTxManagerHibernate getRepositoryTxManagerHibernate(String paymybuddyPropertiesFile) {
 
 		if (repositoryTxManagerHibernate == null) {
 
-			repositoryTxManagerHibernate = new RepositoryTxManagerHibernate(configurationFile);
+			repositoryTxManagerHibernate = new RepositoryTxManagerHibernate(paymybuddyPropertiesFile);
 
 			logger.info("Creation of Tx Hibernate manager : OK");
 		}
@@ -52,17 +51,31 @@ public class RepositoryTxManagerHibernate {
 	/**
 	 * Create an instance of Hibernate SessionFactory.
 	 * 
-	 * @param configurationFile The path of the Hibernate configuration file
+	 * @param configurationFile The path of the paymybuddy properties file
 	 * 
 	 * @return The SessionFactory
 	 */
 	private SessionFactory getSessionFactory() {
 
-		File configFile = new File(hibernateConfigurationFile);
+		Properties paymybuddyProperties = new Properties();
+		try {
+			paymybuddyProperties.load(ClassLoader.getSystemClassLoader().getResourceAsStream(paymybuddyPropertiesFile));
+		} catch (Exception e) {
+			logger.error("Error during load of paymybuddy properties file", e);
+		}
+		// paymybuddyProperties.put(Environment.DRIVER, "org.postgresql.Driver");
+		paymybuddyProperties.put(Environment.SHOW_SQL, "true");
+		paymybuddyProperties.put(Environment.FORMAT_SQL, "true");
+		paymybuddyProperties.put(Environment.HBM2DDL_AUTO, "none");
+		paymybuddyProperties.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
+		paymybuddyProperties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+		paymybuddyProperties.put(Environment.C3P0_MAX_SIZE, "300");
+		paymybuddyProperties.put(Environment.POOL_SIZE, "300");
 
-		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure(configFile).build();
+		Configuration configuration = new Configuration().setProperties(paymybuddyProperties);
 
-		SessionFactory sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+		SessionFactory sessionFactory = configuration.addAnnotatedClass(com.paymybuddy.entity.Transaction.class)
+				.addAnnotatedClass(com.paymybuddy.entity.Utilisateur.class).buildSessionFactory();
 
 		return sessionFactory;
 	}
@@ -89,6 +102,7 @@ public class RepositoryTxManagerHibernate {
 	 */
 	public void closeCurrentSession() {
 		currentSession.close();
+		logger.info("Session closed");
 	}
 
 	/**
@@ -97,6 +111,7 @@ public class RepositoryTxManagerHibernate {
 	public void commitTxAndCloseCurrentSession() {
 		currentTx.commit();
 		currentSession.close();
+		logger.info("Session with Tx closed");
 	}
 
 	/**
