f558a6828cb5d8f2df510c9501d380b7ee010d90
olivier.bonheur <olivier.bonheur@gmail.com>
2020-07-14 01:00:28 +0200

    Work on RepositoryDataSource for tests purpose

--- PayMyBuddy/src/test/java/com/paymybuddy/configuration/RepositoryDataSource.java
+++ PayMyBuddy/src/test/java/com/paymybuddy/configuration/RepositoryDataSource.java
@@ -1,43 +1,74 @@
 package com.paymybuddy.configuration;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.Properties;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 /**
- * Class in charge of the construction of DriverManager DataSource objects for
- * interaction with the database
+ * Class Factory in charge of the construction of DriverManager DataSource
+ * objects for interaction with the database
  */
 public class RepositoryDataSource {
 
 	private static final Logger logger = LoggerFactory.getLogger(RepositoryDataSource.class);
 
+	private static DriverManagerDataSource dataSource = null;
+
+	private static Properties properties = null;
+
+	private static InputStream input = null;
+
+	private static String paymybuddyPropertiesFilePath = null;
+
 	/**
 	 * Create a DriverManager DataSource for the database.
 	 * 
-	 * @param driverClassName The name of the driver class of the database
-	 * 
-	 * @param url             The url of the database
-	 * 
-	 * @param username        The username for the connection to the database
-	 * 
-	 * @param password        The password for the connection to the database
+	 * @param paymybuddyPropertiesFile The paymybuddy properties file
 	 * 
 	 * @return The DriverManagerDataSource
 	 */
-	public static DriverManagerDataSource getDataSource(String driverClassName, String url, String username,
-			String password) {
+	public static DriverManagerDataSource getDataSource(String paymybuddyPropertiesFile) {
 
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		paymybuddyPropertiesFilePath = "src/test/resources/" + paymybuddyPropertiesFile;
 
-		dataSource.setDriverClassName(driverClassName);
+		properties = new Properties();
+
+		try {
+			input = new FileInputStream(paymybuddyPropertiesFilePath);
+			properties.load(input);
+		} catch (IOException ex) {
+			logger.error("Error in loading paymybuddy.properties file", ex);
+		}
+
+		String driver = properties.getProperty("hibernate.connection.driver_class");
+		String url = properties.getProperty("hibernate.connection.url");
+		String username = properties.getProperty("hibernate.connection.username");
+		String password = properties.getProperty("hibernate.connection.password");
+
+		dataSource = new DriverManagerDataSource();
+
+		dataSource.setDriverClassName(driver);
 		dataSource.setUrl(url);
 		dataSource.setUsername(username);
 		dataSource.setPassword(password);
 
-		logger.info("DriverManager DataSource sucessfully created.");
-
 		return dataSource;
 	}
 
+	/**
+	 * Close the DriverManager DataSource.
+	 */
+	public static void closeDatasource() {
+		try {
+			dataSource.getConnection().close();
+		} catch (SQLException e) {
+			logger.error("Error during close of datasource", e);
+		}
+	}
 }
